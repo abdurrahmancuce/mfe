e414120933b47f5f8b3559a37884b56d576aea47
Abdurrahman CUCE <acuce@genband.com>
2023-01-12 17:23:51 +0300

    Added auth pages

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -1,6 +1,6 @@
-import { mount } from "marketing/MarketingApp";
+import { mount } from "auth/AuthApp";
 import React, { useRef, useEffect} from "react";
-import { useHistory } from 'react-router-dom'
+import { useHistory } from 'react-router-dom';
 
 export default () => {
     const ref = useRef(null);
